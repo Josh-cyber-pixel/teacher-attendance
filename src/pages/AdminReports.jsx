e51d58ed90c0672a1e1082896d84bfffaac91495
Joshua Nignamon <joshuanignamon52@gmail.com>
2026-07-03 15:44:14 -1200

    fix: force rename layout to Layout

--- src/pages/AdminReports.jsx
+++ src/pages/AdminReports.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import api from '../api';
 
 export default function AdminReports() {
